a7dab4e7f32eba5c84f441862d0c87f849553df7
phucdn <phucdn@allexceed.co.jp>
2026-07-14 15:26:42 +0700

    [l-travel-blog-202026-b-01][feature/ts-01] 04. Basic create langding page b01

---
+++ app/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hero bg-base-200 container mx-auto mt-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Blog
+        </h1>
+        <p class="py-6">
+          Keep track of your travels anf adventures with this simple travel blog
+        </p>
+        <button class="btn btn-accent">
+          Sign In with GitHUB  <Icon name="tabler:brand-github" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
