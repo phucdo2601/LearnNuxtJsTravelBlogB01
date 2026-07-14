3f1aa7c3d52a06043d6f285ef745feab5dde01ee
Do Ngoc Phuc <65969192+phucdo2601@users.noreply.github.com>
2026-07-14 15:29:22 +0700

    Feature/ts 01 (#4)
    * [l-travel-blog-202026-b-01][feature/ts-01] 02. Basic commit on branch feature/ts-01 for buidling workflow b01
    
    * [l-travel-blog-202026-b-01][feature/ts-01] 03. Basic set up tailwind/ daisy ui/ icons b01
    
    * [l-travel-blog-202026-b-01][feature/ts-01] 04. Basic create langding page b01
    
    ---------
    
    Co-authored-by: phucdn <phucdn@allexceed.co.jp>

---
+++ app/components/app/nav-bar.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+
+};
+</script>
+
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Travel Blog
+      </NuxtLink>
+    </div>
+
+    <div class="navbar-end">
+      <a href="" class="btn btn-accent">Sign In</a>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
