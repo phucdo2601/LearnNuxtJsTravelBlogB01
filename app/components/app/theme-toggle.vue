017d9d308c407fb3d63578f1e4f650efc78b5a5a
Do Ngoc Phuc <65969192+phucdo2601@users.noreply.github.com>
2026-07-14 16:09:44 +0700

    Feature/ts 01 (#5)
    * [l-travel-blog-202026-b-01][feature/ts-01] 02. Basic commit on branch feature/ts-01 for buidling workflow b01
    
    * [l-travel-blog-202026-b-01][feature/ts-01] 03. Basic set up tailwind/ daisy ui/ icons b01
    
    * [l-travel-blog-202026-b-01][feature/ts-01] 04. Basic create langding page b01
    
    * [l-travel-blog-202026-b-01][feature/ts-01] 05. Basic config theme toggle b01
    
    ---------
    
    Co-authored-by: phucdn <phucdn@allexceed.co.jp>

---
+++ app/components/app/theme-toggle.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set(value) {
+    colorMode.preference = value ? "dark" : "night";
+  },
+});
+</script>
+
+<template>
+  <label class="swap swap-rotate">
+    <!-- this hidden checkbox controls the state -->
+    <input v-model="isDark" type="checkbox">
+
+    <!-- sun icon -->
+    <Icon
+      class="swap-on"
+      name="tabler:sun"
+      :size="24"
+    />
+
+    <!-- moon icon -->
+    <Icon
+      class="swap-off"
+      name="tabler:moon-filled"
+      :size="24"
+    />
+  </label>
+</template>
+
+<style lang="scss" scoped></style>
