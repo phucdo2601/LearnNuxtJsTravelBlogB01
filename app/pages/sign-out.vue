258f420916d068abe7e5acb626553d8ba0b3a0e1
Do Ngoc Phuc <65969192+phucdo2601@users.noreply.github.com>
2026-07-15 13:16:56 +0700

    Feature/ts 01 (#9)
    * [l-travel-blog-202026-b-01][feature/ts-01] 02. Basic commit on branch feature/ts-01 for buidling workflow b01
    
    * [l-travel-blog-202026-b-01][feature/ts-01] 03. Basic set up tailwind/ daisy ui/ icons b01
    
    * [l-travel-blog-202026-b-01][feature/ts-01] 04. Basic create langding page b01
    
    * [l-travel-blog-202026-b-01][feature/ts-01] 05. Basic config theme toggle b01
    
    * [l-travel-blog-202026-b-01][feature/ts-01] 06. Basic update config orm drizzle with turso b01
    
    * [l-travel-blog-202026-b-01][feature/ts-01] 06-1. Basic update table layoout relation ship b01
    
    * [l-travel-blog-202026-b-01][feature/ts-01] 06-3. Basic update config orm drizzle and add schema with turso b01
    
    * [l-travel-blog-202026-b-01][feature/ts-01] 06-4. Basic update config deploy setting b01
    
    * [l-travel-blog-202026-b-01][feature/ts-01] 07. Basic config better auth b01
    
    * [l-travel-blog-202026-b-01][feature/ts-01] 08. Basic config login with github b01
    
    * [l-travel-blog-202026-b-01][feature/ts-01] 08-1. Basic config login with github b02
    
    * [l-travel-blog-202026-b-01][feature/ts-01] 09. Basic config signout func b01
    
    ---------
    
    Co-authored-by: phucdn <phucdn@allexceed.co.jp>

---
+++ app/pages/sign-out.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.signOut();
+});
+</script>
+
+<template>
+  <div
+    class="card bgg-base-300 container mt-4 min-h-72 text-center mx-auto flex flex-col justify-center items-center gap-4"
+  >
+    <span class="loading loading-spinner loading-xl" />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
