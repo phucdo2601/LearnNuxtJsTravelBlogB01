06583112c41fbf5d31ad98c68b7714cbb6054280
phucdn <phucdn@allexceed.co.jp>
2026-07-15 07:59:26 +0700

    [l-travel-blog-202026-b-01][feature/ts-01] 06-3. Basic update config orm drizzle and add schema with turso b01

---
+++ app/lib/db/schema/location-log-image.ts
@@ -0,0 +1,13 @@
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+import { locationLog } from "./location-log";
+
+export const locationLogImage = sqliteTable("locationLogImage", {
+  id: int().primaryKey({
+    autoIncrement: true,
+  }),
+  key: text().notNull(),
+  locationId: int().notNull().references(() => locationLog.id),
+  createdAt: int().notNull().$default(() => Date.now()),
+  updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
+});
