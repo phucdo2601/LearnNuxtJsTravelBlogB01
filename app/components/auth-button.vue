63632e0d255403c2a1e1a7bdf78ccd3400c8b622
phucdn <phucdn@allexceed.co.jp>
2026-07-15 13:14:24 +0700

    [l-travel-blog-202026-b-01][feature/ts-01] 09. Basic config signout func b01

--- app/components/auth-button.vue
+++ app/components/auth-button.vue
@@ -5,11 +5,37 @@ const authStore = useAuthStore();
 
 const {
   loading,
+  user,
 } = storeToRefs(authStore);
 </script>
 
 <template>
+  <div v-if="!authStore.loading && user" class="dropdown dropdown-end">
+    <div
+      tabindex="0"
+      role="button"
+      class="btn m-1"
+    >
+      <div v-if="user.image" class="avatar">
+        <div class="w-8 rounded-full">
+          <img :src="user.image" :alt="user.name">
+        </div>
+      </div>
+      {{ user.name }}
+    </div>
+    <ul tabindex="-1" class="dropdown-content menu bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
+      <li>
+        <NuxtLink to="/sign-out">
+          <Icon name="tabler:logout-2" size="24" />
+          Sign Out
+        </NuxtLink>
+      </li>
+      <li><a>Item 2</a></li>
+    </ul>
+  </div>
+
   <button
+    v-else
     :disabled="loading"
     class="btn btn-accent"
     @click="authStore.signIn"
